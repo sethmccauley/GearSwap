36f6d55d297efb89909af185b9eb82c386eebbc2
Seth McCauley <sethmccauley@users.noreply.github.com>
2017-12-01 15:40:24 -0700

    Add files via upload

---
+++ determine_haste.lua
@@ -0,0 +1,76 @@
+function determine_haste_group()
+   
+	classes.CustomMeleeGroups:clear()
+	h = 0
+	-- Spell Haste 15/30
+	if buffactive[33] then
+		if state.HasteMode.value == 'Haste I' then
+			h = h + 15
+		elseif state.HasteMode.value == 'Haste II' then
+			h = h + 30
+		end
+	end
+	-- Geo Haste 30
+	if buffactive[580] then
+		h = h + 30
+	end
+	-- Mighty Guard 15
+	if buffactive[604] then
+		h = h + 15
+	end
+	-- Embrava 15
+	if buffactive.embrava then
+		h = h + 21
+	end
+	-- March(es) 
+	if buffactive.march then
+		if state.MarchMode.value == 'Honor' then
+			if buffactive.march == 2 then
+				h = h + 27 + 16
+			elseif buffactive.march == 1 then
+				h = h + 16
+			elseif buffactive.march == 3 then
+				h = h + 27 + 17 + 16
+			end
+		elseif state.MarchMode.value == 'Trusts' then
+			if buffactive.march == 2 then
+				h = h + 26
+			elseif buffactive.march == 1 then
+				h = h + 16
+			elseif buffactive.march == 3 then
+				h = h + 27 + 17 + 16
+			end
+		elseif state.MarchMode.value == '7' then
+			if buffactive.march == 2 then
+				h = h + 27 + 17
+			elseif buffactive.march == 1 then
+				h = h + 27
+			elseif buffactive.march == 3 then
+				h = h + 27 + 17 + 16
+			end
+		elseif state.MarchMode.value == '3' then
+			if buffactive.march == 2 then
+				h = h + 13.5 + 20.6
+			elseif buffactive.march == 1 then
+				h = h + 20.6
+			elseif buffactive.march == 3 then
+				h = h + 27 + 17 + 16
+			end
+		end
+	end
+
+	-- Determine CustomMeleeGroups
+	if h >= 15 and h < 30 then 
+		classes.CustomMeleeGroups:append('Haste_15')
+		add_to_chat('Haste Group: 15% -- From Haste Total: '..h)
+	elseif h >= 30 and h < 35 then 
+		classes.CustomMeleeGroups:append('Haste_30')
+		add_to_chat('Haste Group: 30% -- From Haste Total: '..h)
+	elseif h >= 35 and h < 40 then 
+		classes.CustomMeleeGroups:append('Haste_35')
+		add_to_chat('Haste Group: 35% -- From Haste Total: '..h)
+	elseif h >= 40 then
+		classes.CustomMeleeGroups:append('MaxHaste')
+		add_to_chat('Haste Group: Max -- From Haste Total: '..h)
+	end
+end
