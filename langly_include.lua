40634a80ad41ef5fc762f405d05bcab4758466df
Seth McCauley <sethmccauley@users.noreply.github.com>
2018-01-18 14:53:02 -0700

    Create langly_include.lua

---
+++ langly_include.lua
@@ -0,0 +1,51 @@
+res = require('resources')
+extdata = require('extdata')
+
+-- Item must be equipped for it to return any meaningful value.
+function is_enchant_ready(--[[name of item]]item)
+	local item_id, item = res.items:find(function(v) if v.name == item then return true end end)
+	local inventory = windower.ffxi.get_items()
+	local usable_bags = T{'inventory','wardrobe','wardrobe2','wardrobe3','wardrobe4'}
+	local itemdata = {}
+	
+	for i,v in pairs(inventory) do
+		if usable_bags:contains(i) then
+			for key,val in pairs(v) do
+				if type(val) == 'table' and val.id == item_id then
+					itemdata = extdata.decode(val)
+				end
+			end
+		end
+	end
+	
+	if itemdata and itemdata.charges_remaining then
+		if itemdata.activation_time - itemdata.next_use_time > item.cast_delay then
+			return true
+		end
+	end
+	return false
+end
+
+function has_charges(--[[name of item]]item)
+	local item_id, item = res.items:find(function(v) if v.name == item then return true end end)
+	local inventory = windower.ffxi.get_items()
+	local bags = T{'inventory','safe','safe2','storage','satchel','locker','sack','case','wardrobe','wardrobe2','wardrobe3','wardrobe4'}
+	local itemdata = {}
+	
+	for i,v in pairs(inventory) do
+		if bags:contains(i) then
+			for key,val in pairs(v) do
+				if type(val) == 'table' and val.id == item_id then
+					itemdata = extdata.decode(val)
+				end
+			end
+		end
+	end
+	
+	if itemdata and itemdata.charges_remaining then
+		if itemdata.charges_remaining > 0 then
+			return true
+		end
+	end
+	return false
+end
